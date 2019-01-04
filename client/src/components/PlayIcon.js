3af104c626cd1505c86526a0397aa67a8b25051f
Matt Holland <mtholland10@gmail.com>
2019-01-04 14:14:09 -0600

    Use PureComponent for PlayIconContainer, use memo for play & pause icons, clean up pause & play click handlers

--- client/src/components/PlayIcon.js
+++ client/src/components/PlayIcon.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
+import React, { memo } from 'react';
 
-class PlayIcon extends Component {
-  constructor(props) {
-    super(props);
+const PlayIcon = memo(function PlayIcon(props) {
+  const { onClickPlay } = props;
 
-    this.state = {
-      isTrackActive: false,
-    }
-  }
-
-  render() {
-    const { onClickPlay } = this.props;
-
-    return (
-      <svg className="icon-play" onClick={onClickPlay} xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMin" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-3 17v-10l9 5.146-9 4.854z"/></svg>
-    );
-  }
-}
+  return (
+    <svg className="icon-play" onClick={onClickPlay} xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMin" width="24" height="24" viewBox="0 0 24 24">
+      <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-3 17v-10l9 5.146-9 4.854z"/>
+    </svg>
+  );
+});
 
 export default PlayIcon;
