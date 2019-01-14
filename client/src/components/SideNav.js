ca11227e810e5269be741f6ec53a72812b28b9bc
Matt Holland <mtholland10@gmail.com>
2019-01-13 22:38:42 -0600

    Add close and open icons, click handlers, transitions, media queries

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -1,95 +1,124 @@
 /**
- * Create the side nav, used by several views
+ * Create the side nav, used by all main container views
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 
-// Render the side nav list items
-function renderList(selectionArr, selectedId, navType, handleSelect) {
-  const listItems = selectionArr.map((selectedObj, i) => {
-    const { name, id, itemType } = selectedObj;
-    const activeClassName = (selectedId === id ? 'active' : '');
-    let clickParam;
+class SideNav extends Component {
+  constructor(props) {
+    super(props);
 
-    // Passed param will vary depending on what type of view our nav is residing in
-    // name is used for card container title display
+    this.state = {
+      isSideNavActive: false,
+    };
+
+    this.handleSideNavClose = this.handleSideNavClose.bind(this);
+  }
+
+  // Render the side nav list items
+  // navType used to flag current view for dictating click action param, & title icon
+  renderListItems() {
+    const {
+      navType,
+      handleSelect,
+      selectedId,
+      selectionArr,
+    } = this.props;
+
+    const listItems = selectionArr.map((selectedObj, i) => {
+      const { name, id, itemType } = selectedObj;
+      const activeClassName = (selectedId === id ? 'active' : '');
+      let clickParam;
+
+      // Passed param will vary depending on what type of view our nav is residing in
+      // name is used for card container title display
+      switch(navType) {
+        case 'new-release-market':
+          clickParam = id;
+          break;
+        case 'album-search':
+          clickParam = name;
+          break;
+        case 'playlist':
+          clickParam = id;
+        case 'detail-container':
+          clickParam = id;
+        default:
+          break;
+      }
+
+      return (
+        <li
+          className={`side-nav__list-item ${activeClassName}`}
+          key={i}
+          onClick={() => handleSelect(clickParam, selectedObj, itemType)}>
+          {name}
+        </li>
+      );
+    });
+
+    return listItems;
+  };
+
+  // Change side nav title icon depending on view type
+  renderTitleIcon() {
+    const { navType } = this.props;
+
+    let iconName;
+
     switch(navType) {
       case 'new-release-market':
-        clickParam = id;
+        iconName = 'fa-map';
         break;
       case 'album-search':
-        clickParam = name;
+        iconName = 'fa-search';
         break;
       case 'playlist':
-        clickParam = id;
+        iconName = 'fa-list-ul';
+        break;
       case 'detail-container':
-        clickParam = id;
+        iconName = 'fa-play-circle';
+        break;
       default:
+        iconName = 'fa-map';
         break;
     }
 
-    return (
-      <li
-        className={`side-nav__list-item ${activeClassName}`}
-        key={i}
-        onClick={() => handleSelect(clickParam, selectedObj, itemType)}>
-        {name}
-      </li>
-    );
-  });
-
-  return listItems;
-};
-
-// Change side nav title icon depending on view type
-function renderTitleIcon(navType) {
-  let iconName;
-
-  switch(navType) {
-    case 'new-release-market':
-      iconName = 'fa-map';
-      break;
-    case 'album-search':
-      iconName = 'fa-search';
-      break;
-    case 'playlist':
-      iconName = 'fa-list-ul';
-      break;
-    case 'detail-container':
-      iconName = 'fa-play-circle';
-      break;
-    default:
-      iconName = 'fa-map';
-      break;
+    return <i className={`fas ${iconName}`}></i>;
   }
 
-  return (
-    <i className={`fas ${iconName}`}></i>
-  );
-}
+  // Toggle the true/false state
+  handleSideNavClose() {
+    const { isSideNavActive } = this.state;
 
-const SideNav = (props) => {
-  // navType used to flag current view for dictating click action param, & title icon
-  const {
-    handleSelect,
-    selectedId,
-    selectionArr,
-    title,
-    navType,
-  } = props;
+    this.setState({
+      isSideNavActive: !isSideNavActive,
+    });
+  }
 
-  return (
-    <div className="side-nav">
-      <div className="side-nav__img"></div>
-      <div className="side-nav__title">tuneIn</div>
-      <div className="side-nav__header">{title}
-        {renderTitleIcon(navType)}
+  render() {
+    const { title } = this.props;
+    const { isSideNavActive } = this.state;
+
+    const activeClassName = isSideNavActive ? 'side-nav--active' : null;
+    const hiddenStyle = isSideNavActive ? 'hidden' : null;
+
+    return (
+      <div className={`side-nav ${activeClassName}`}>
+        <i class="far fa-times-circle" onClick={this.handleSideNavClose}></i>
+        <div className="side-nav__img"></div>
+        <div className="side-nav__title">tuneIn
+          <i class={`fas fa-bars ${hiddenStyle}`} onClick={this.handleSideNavClose}></i>
+        </div>
+        <div className="side-nav__header">{title}
+          {this.renderTitleIcon()}
+        </div>
+        <ul className="side-nav__list">
+          {this.renderListItems()}
+        </ul>
       </div>
-      <ul className="side-nav__list">
-        {renderList(selectionArr, selectedId, navType, handleSelect)}
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
 export default SideNav;
