bcfdea4bccdde8b5796dd38485aa5a51105e8c0a
Matt Holland <mtholland10@gmail.com>
2019-02-06 15:08:19 -0600

    Add dropdown sass components, add new ProfileAvatar component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
+import ProfileAvatar from './ProfileAvatar';
+
 class NavBar extends Component {
   // App's entry point is DetailContainer; make sure nav item has active class on initial oauth callback route
   isNavDetailLinkActive(match, { pathname }) {
@@ -15,6 +17,7 @@ class NavBar extends Component {
         <NavLink className="navbar__link" to="/playlists"><i className="fas fa-book-open"></i> Playlists</NavLink>
         <NavLink className="navbar__link" to="/detail" isActive={this.isNavDetailLinkActive}><i className="fas fa-headphones-alt"></i> Now Playing</NavLink>
         <NavLink className="navbar__link" to="/search"><i className="fas fa-search"></i> Search</NavLink>
+        <ProfileAvatar />
       </div>
     );
   }
---
+++ client/src/components/ProfileAvatar.js
@@ -0,0 +1,55 @@
+/**
+ *  Main profile navbar avatar and dropdown
+ */
+
+import React, { Component } from 'react';
+
+class ProfileAvatar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDropdownActive: false,
+    };
+
+    this.onClickAvatar = this.onClickAvatar.bind(this);
+  }
+
+  // Activate/deactivate the dropdown
+  onClickAvatar() {
+    const { isDropdownActive } = this.state;
+
+    this.setState({
+      isDropdownActive: !isDropdownActive,
+    });
+  }
+
+  render() {
+    const { isDropdownActive } = this.state;
+
+    const dropdownActiveClass = isDropdownActive ? 'dropdown--active' : '';
+
+    return (
+      <div onClick={this.onClickAvatar} class="avatar">
+        <div className={`dropdown ${dropdownActiveClass}`}>
+          <ul className="dropdown__list">
+            <li className="dropdown__list-item">
+              <span className="dropdown__icon"><i className="far fa-user"></i></span>
+              <a href="https://www.mtholla.com" target="_blank" className="dropdown__title">about matt h</a>
+            </li>
+            <li className="dropdown__list-item">
+              <span className="dropdown__icon"><i className="fas fa-clipboard-list"></i></span>
+              <a href="https://www.medium.com/mtholla" target="_blank" className="dropdown__title">blog site</a>
+            </li>
+            <li className="dropdown__list-item">
+              <span className="dropdown__icon"><i className="fas fa-sign-out-alt"></i></span>
+              <a href="https://www.linkedin.com/in/matt-holland/" target="_blank" className="dropdown__title">linkedIn profile</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProfileAvatar;
